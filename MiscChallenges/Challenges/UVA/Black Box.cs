e1978c8b04196652456eb0db2e4b28bb28922f31
darrellp <darrellp@7942be1d-c94a-443e-a36d-56657dcc11f9>
2015-07-09 21:45:24 +0000

    Moved UVA challenges to new protocol

---
+++ MiscChallenges/Challenges/UVA/Black Box.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using Priority_Queue;
+
+namespace MiscChallenges.Challenges
+{
+	public static partial class ChallengeClass
+	{
+		[Challenge("UVA", "Black Box",
+			"https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=7&page=show_problem&problem=442")]
+		// ReSharper disable once InconsistentNaming
+		public class BlackBox : IChallenge
+		{
+			public void Solve()
+			{
+				var nCases = GetVal();
+				bool firstCase = true;
+				for (var iCase = 0; iCase < nCases; iCase++)
+				{
+					// Skip the blank line between cases in the input
+					Console.ReadLine();
+
+					// Write the blank line between output cases if necessary
+					if (!firstCase)
+					{
+						Console.WriteLine();
+					}
+					firstCase = false;
+
+					// Retrieve the next case
+					var caseCur = new BlackBoxCase();
+
+					// Write each value in the solution on a separate line
+					foreach (var getValue in caseCur.Solve())
+					{
+						Console.WriteLine(@"{0}", getValue);
+					}
+				}
+			}
+
+			public class BlackBoxCase
+			{
+				private readonly List<int> _added;
+				private readonly List<int> _getCounts;
+ 
+				public BlackBoxCase()
+				{
+					Console.ReadLine();		// We don't really need to know these numbers - they're implicit
+					_added = GetVals();
+					_getCounts = GetVals();
+				}
+
+				public IEnumerable<int> Solve()
+				{
+					var pq = new NthPriorityQueue<int>();
+					var iAdd = 0;
+
+					foreach (int t in _getCounts)
+					{
+						for (; iAdd < t; iAdd++)
+						{
+							pq.Add(_added[iAdd]);
+						}
+						yield return pq.Peek();
+						pq.N++;
+					}
+				}
+			}
+
+			/// <summary>
+			/// Keep a priority queue where we pull the n'th smallest rather than the smallest
+			/// </summary>
+			public class NthPriorityQueue<T> where T : IComparable
+			{
+				readonly BinaryPriorityQueue<T> _smallest = new BinaryPriorityQueue<T>((n1, n2) => n2.CompareTo(n1));
+				readonly BinaryPriorityQueue<T> _largest = new BinaryPriorityQueue<T>();
+				private int _n;
+
+				public NthPriorityQueue()
+				{
+					_n = 0;
+				}
+
+				public NthPriorityQueue(int n)
+				{
+					_n = n;
+				}
+
+				public int N
+				{
+					get { return _n; }
+					set
+					{
+						if (value < 0)
+						{
+							throw new ArgumentException("Invalid N in NthPriorityQueue");
+						}
+						if (value == _n)
+						{
+							return;
+						}
+
+						var oldN = _n;
+						_n = value;
+						if (oldN < _n)
+						{
+							while (oldN < _n && _largest.Count > 0)
+							{
+								_smallest.Add(_largest.Pop());
+								oldN++;
+							}
+						}
+						else
+						{
+							while (oldN > _n)
+							{
+								_largest.Add(_smallest.Pop());
+								oldN--;
+							}
+						}
+					}
+				}
+
+				public void Add(T val)
+				{
+					if (_smallest.Count < N)
+					{
+						_smallest.Add(val);
+						return;
+					}
+					if (N != 0 && val.CompareTo(_smallest.Peek()) < 0)
+					{
+						_smallest.Add(val);
+						val = _smallest.Pop();
+					}
+					_largest.Add(val);
+				}
+
+				public T Peek()
+				{
+					return _largest.Peek();
+				}
+
+				public T Pop()
+				{
+					return _largest.Pop();
+				}
+			}
+
+			public string RetrieveSampleInput()
+			{
+				return @"
+1
+
+7 4
+3 1 -4 2 8 -1000 2
+1 2 6 6
+";
+			}
+
+			public string RetrieveSampleOutput()
+			{
+				return @"
+3
+3
+1
+2
+";
+			}
+		}
+	}
+}
---
+++ MiscChallenges/Challenges/UVA/palindromes.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiscChallenges.Challenges
+{
+	public static partial class ChallengeClass
+	{
+		/// <summary>
+		/// </summary>
+		[Challenge("UVA", "Palindromes",
+			"https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=6&page=show_problem&problem=342")]
+		// ReSharper disable once InconsistentNaming
+		public class Palindromes : IChallenge
+		{
+			private enum PalType
+			{
+				NotPal,
+				Regular,
+				Mirrored,
+				MirroredPal
+			};
+
+			private readonly Dictionary<char, char> _letterMirrors = new Dictionary<char, char>
+			{
+				{'A', 'A'}, {'E', '3'}, {'H', 'H'}, {'I', 'I'},
+				{'J', 'L'}, {'L', 'J'}, {'Z', '5'}, {'O', 'O'},
+				{'S', '2'}, {'T', 'T'}, {'U', 'U'}, {'V', 'V'},
+				{'W', 'W'}, {'X', 'X'}, {'Y', 'Y'}, {'1', '1'},
+				{'2', 'S'}, {'3', 'E'}, {'5', 'Z'}, {'8', '8'}
+			};
+
+			public void Solve()
+			{
+				string nextString;
+				while ((nextString = Console.ReadLine()) != null)
+				{
+					var tag = string.Empty;
+					switch (CheckPalType(nextString))
+					{
+						case PalType.NotPal:
+							tag = "not a palindrome";
+							break;
+
+						case PalType.Regular:
+							tag = "a regular palindrome";
+							break;
+
+						case PalType.Mirrored:
+							tag = "a mirrored string";
+							break;
+
+						case PalType.MirroredPal:
+							tag = "a mirrored palindrome";
+							break;
+					}
+					Console.WriteLine(@"{0} -- is {1}.", nextString, tag);
+					// Because of the weird "after each output line, you must print an empty line"
+					// requirement.  I'm interepreting this literally - i.e., EACH output line,
+					// including the last one.
+					Console.WriteLine();
+				}
+			}
+
+			private PalType CheckPalType(string s)
+			{
+				bool fIsMirrored = true;
+				bool fIsRegular = true;
+				for (var ich = 0; ich < s.Length/2; ich++)
+				{
+					var ichBack = s.Length - ich - 1;
+					if (s[ichBack] != s[ich])
+					{
+						fIsRegular = false;
+					}
+					if (!_letterMirrors.ContainsKey(s[ich]) || s[ichBack] != _letterMirrors[s[ich]])
+					{
+						fIsMirrored = false;
+					}
+					if (!fIsMirrored && !fIsRegular)
+					{
+						return PalType.NotPal;
+					}
+				}
+				if (fIsMirrored && fIsRegular)
+				{
+					return PalType.MirroredPal;
+				}
+				return fIsMirrored ? PalType.Mirrored : PalType.Regular;
+			}
+
+			public string RetrieveSampleInput()
+			{
+				return @"
+NOTAPALINDROME
+ISAPALINILAPASI
+2A3MEAS
+ATOYOTA
+";
+			}
+
+			public string RetrieveSampleOutput()
+			{
+				return @"
+NOTAPALINDROME -- is not a palindrome.
+
+ISAPALINILAPASI -- is a regular palindrome.
+
+2A3MEAS -- is a mirrored string.
+
+ATOYOTA -- is a mirrored palindrome.
+
+";
+			}
+		}
+	}
+}
