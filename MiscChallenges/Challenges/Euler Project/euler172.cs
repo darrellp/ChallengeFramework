824648d0884dd71d32af111e89990a4950b75b03
darrellp <darrellp@7942be1d-c94a-443e-a36d-56657dcc11f9>
2015-07-09 19:30:04 +0000

    Added Euler problems with new protocol

---
+++ MiscChallenges/Challenges/Euler Project/euler172.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace MiscChallenges.Challenges
+{
+	public static partial class ChallengeClass
+	{
+		[Challenge("Euler Project", "Prob 172", "https://projecteuler.net/problem=172")]
+		// ReSharper disable once InconsistentNaming
+		public class Euler172 : IChallenge
+		{
+			public void Solve()
+			{
+				var ret = 0UL;
+				var fact18 = Fact(18);
+
+				for (var m3 = 0; m3 <= 6; m3++)
+				{
+					for (var m2 = 0; m2 <= 9; m2++)
+					{
+						var m1 = 18 - 3 * m3 - 2 * m2;
+						if (m1 < 0 || m1 + m2 + m3 > 10)
+						{
+							continue;
+						}
+						ret += (fact18 / ((ulong)Math.Pow(6L, m3) * (ulong)Math.Pow(2L, m2))) *
+							Comb(10, m1) * Comb(10 - m1, m2) * Comb(10 - m1 - m2, m3);
+					}
+				}
+				Console.WriteLine(ret * 9 / 10);
+			}
+
+			public string RetrieveSampleInput() { return null; }
+			public string RetrieveSampleOutput()
+			{
+				return @"
+227485267000992000
+";
+			}
+		}
+	}
+}
---
+++ MiscChallenges/Challenges/Euler Project/euler173.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace MiscChallenges.Challenges
+{
+	public static partial class ChallengeClass
+	{
+		[Challenge("Euler Project", "Prob 173", "https://projecteuler.net/problem=173")]
+		public class Euler173 : IChallenge
+		{
+			public static class Static173
+			{
+				public static int Laminae(int holeSize, int tileCount)
+				{
+					return Math.Max(0, (int)Math.Floor((Math.Sqrt((double)holeSize * holeSize + tileCount) - holeSize) / 2.0));
+				}
+			}
+
+			public void Solve()
+			{
+				var totalLaminae = 0;
+				var tileCount = 1000000;
+
+				for (var holeLayers = 1; holeLayers <= tileCount / 4; holeLayers++)
+				{
+					totalLaminae += Static173.Laminae(holeLayers, tileCount);
+				}
+				Console.WriteLine(totalLaminae);
+			}
+
+			public string RetrieveSampleInput() { return null; }
+			public string RetrieveSampleOutput()
+			{
+				return @"
+1572729
+";
+			}
+		}
+	}
+}
