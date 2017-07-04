c9a827b6fd7fc97fe1a327c67836bb2222d0c21f
Darrell Plank <jar_czar@msn.com>
2017-07-04 03:22:47 -0700

    Added some Sphere challenges

---
+++ MiscChallenges/Challenges/Sphere/LifeTheUniverseAndEverything.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+namespace MiscChallenges.Challenges
+{
+    public static partial class ChallengeClass
+    {
+        /// <summary>
+        /// </summary>
+        [Challenge("Sphere", "Life, the Universe and Everything",
+            "http://www.spoj.com/problems/TEST/")]
+        // ReSharper disable once InconsistentNaming
+        public class LifeTheUniverseAndEverything : IChallenge
+        {
+            public void Solve()
+            {
+                while (true)
+                {
+                    var inLine = ReadLine();
+                    if (inLine == null || inLine == "42")
+                    {
+                        break;
+                    }
+                    WriteLine(inLine);
+                }
+            }
+
+            public string RetrieveSampleInput()
+            {
+                return @"
+1
+2
+88
+42
+99
+";
+            }
+
+            public string RetrieveSampleOutput()
+            {
+                return @"
+1
+2
+88
+";
+            }
+        }
+    }
+}
---
+++ MiscChallenges/Challenges/Sphere/Prime Generator.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace MiscChallenges.Challenges
+{
+    public static partial class ChallengeClass
+    {
+        /// <summary>
+        /// </summary>
+        [Challenge("Sphere", "Prime Generator",
+            "http://www.spoj.com/problems/PRIME1/")]
+        // ReSharper disable once InconsistentNaming
+        public class PrimeGenerator : IChallenge
+        {
+            static int _highWaterMark = 2;
+            static readonly BitArray IsCompositeList = new BitArray(3);
+
+            public void Solve()
+            {
+                bool isFirstTest = true;
+                IsCompositeList[0] = IsCompositeList[1] = true;
+                ReadLine();
+                List<int> vals;
+                while ((vals = GetVals()) != null)
+                {
+                    var low = vals[0];
+                    var high = vals[1];
+                    if (low <= 1)
+                    {
+                        low = 2;
+                    }
+                    if (high <= 1)
+                    {
+                        high = 2;
+                    }
+                    var composites = new BitArray(high - low + 1);
+
+                    Sieve(composites, low, high);
+
+                    if (!isFirstTest)
+                    {
+                        WriteLine();
+                    }
+                    for (long i = low; i <= high; i++)
+                    {
+                        if (!composites[(int)(i - low)])
+                        {
+                            WriteLine(i);
+                        }
+                    }
+                    isFirstTest = false;
+                }
+            }
+
+            static void SieveValueFromRange(BitArray composites, long offset, long low, long high, int prime)
+            {
+                var rem = low % prime;
+                if (rem == 0)
+                {
+                    rem = low;
+                }
+                var valCur = low + prime - rem;
+                if (valCur == prime)
+                {
+                    valCur *= 2;
+                }
+                var index = (int)(valCur - offset);
+                while (index <= high - offset)
+                {
+                    composites[index] = true;
+                    index += prime;
+                }
+            }
+
+            void ExtendPrimeSieveList(int high)
+            {
+                if (high <= _highWaterMark)
+                {
+                    return;
+                }
+                var oldHighWater = _highWaterMark;
+                _highWaterMark = high;
+
+                IsCompositeList.Length = high + 1;
+                var iCurPrime = 2;
+                var limit = (int)(Math.Sqrt(high) + 1);
+                while (iCurPrime <= limit)
+                {
+                    SieveValueFromRange(IsCompositeList, 0, oldHighWater, _highWaterMark, iCurPrime++);
+                    while (IsCompositeList[iCurPrime] && ++iCurPrime <= high)
+                    {
+                    }
+                }
+            }
+
+            void Sieve(BitArray composites, long low, long high)
+            {
+                var limit = (int)Math.Sqrt(high);
+                ExtendPrimeSieveList(limit);
+                var iCurPrime = 2;
+                while (iCurPrime <= limit)
+                {
+                    SieveValueFromRange(composites, low, low, high, iCurPrime++);
+                    while (iCurPrime <= _highWaterMark && IsCompositeList[iCurPrime])
+                    {
+                        iCurPrime++;
+                    }
+                }
+            }
+
+            public string RetrieveSampleInput()
+            {
+                return @"
+3
+4 6
+1 10
+3 5
+";
+            }
+
+            public string RetrieveSampleOutput()
+            {
+                return @"
+5
+
+2
+3
+5
+7
+
+3
+5
+";
+            }
+        }
+    }
+}
