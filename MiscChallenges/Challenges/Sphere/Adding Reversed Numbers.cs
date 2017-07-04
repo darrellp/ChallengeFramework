277044a6ab91bfad5c4e05c2a203cae8ba065087
Darrell Plank <jar_czar@msn.com>
2017-07-04 15:09:52 -0700

    Added Reversed numbers problem from Sphere

---
+++ MiscChallenges/Challenges/Sphere/Adding Reversed Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace MiscChallenges.Challenges
+{
+    public static partial class ChallengeClass
+    {
+        /// <summary>
+        /// </summary>
+        [Challenge("Sphere", "Adding Reversed Numbers", "http://www.spoj.com/problems/ADDREV/")]
+        // ReSharper disable once InconsistentNaming
+        public class AddingReversedNumbers : IChallenge
+        {
+            public void Solve()
+            {
+                ReadLine();
+                List<int> nextVals;
+                while ((nextVals = GetVals()) != null)
+                {
+                    var v1 = int.Parse(new string(nextVals[0].ToString().Reverse().ToArray()));
+                    var v2 = int.Parse(new string(nextVals[1].ToString().Reverse().ToArray()));
+                    var answer = int.Parse(new string((v1 + v2).ToString().Reverse().ToArray()));
+
+                    WriteLine(answer);
+                }
+            }
+
+            public string RetrieveSampleInput()
+            {
+                return @"
+3
+24 1
+4358 754
+305 794
+";
+            }
+
+            public string RetrieveSampleOutput()
+            {
+                return @"
+34
+1998
+1
+";
+            }
+        }
+    }
+}
