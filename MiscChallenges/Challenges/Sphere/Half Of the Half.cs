f25b489ef0c6b220d9ec57e600b2a6dc516c0955
Darrell Plank <jar_czar@msn.com>
2017-07-04 18:24:24 -0700

    Few more Sphere problems added

---
+++ MiscChallenges/Challenges/Sphere/Half Of the Half.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+namespace MiscChallenges.Challenges
+{
+    public static partial class ChallengeClass
+    {
+        /// <summary>
+        /// </summary>
+        [Challenge("Sphere", "Half of the Half",
+            "http://www.spoj.com/problems/STRHH/")]
+        public class HalfOfTheHalf : IChallenge
+        {
+            public void Solve()
+            {
+                ReadLine();
+                string inLine;
+                while ((inLine = ReadLine()) != null)
+                {
+                    for (int i = 0; i < inLine.Length / 2; i += 2)
+                    {
+                        Write(inLine[i]);
+                    }
+                    WriteLine();
+                }
+            }
+
+            public string RetrieveSampleInput()
+            {
+                return @"
+4
+your 
+progress 
+is 
+noticeable
+";
+            }
+
+            public string RetrieveSampleOutput()
+            {
+                return @"
+y
+po
+i
+ntc
+";
+            }
+        }
+    }
+}
---
+++ MiscChallenges/Challenges/Sphere/Series.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+
+namespace MiscChallenges.Challenges
+{
+    public static partial class ChallengeClass
+    {
+        /// <summary>
+        /// </summary>
+        [Challenge("Sphere", "Series",
+            "http://www.spoj.com/INDCNT1/problems/SEPR/")]
+        public class Series : IChallenge
+        {
+            public void Solve()
+            {
+                ReadLine();
+                var i = 1;
+                while (true)
+                {
+                    var inLine = ReadLine();
+                    if (inLine == null)
+                    {
+                        break;
+                    }
+                    var testVal = (long) int.Parse(inLine);
+                    var output = testVal * (testVal + 1) * (testVal + 2) / 6;
+                    WriteLine($"Query {i++}: {output}");
+                }
+            }
+
+            public string RetrieveSampleInput()
+            {
+                return @"
+3
+1
+2
+5
+";
+            }
+
+            public string RetrieveSampleOutput()
+            {
+                return @"
+Query 1: 1
+Query 2: 4
+Query 3: 35
+";
+            }
+        }
+    }
+}
